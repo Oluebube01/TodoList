27dbfb7c42775e844665c5bba741125d89bf6318
Faith Amarachi M. <oluebube.madu@yahoo.com>
2024-09-19 19:05:27 +0100

    Update ToDoList.jsx

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 function ToDoList(){
 
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState();
     const [newTask, setNewTask] = useState("");
 
     const [check, setCheck] = useState(false);
